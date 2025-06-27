c216a8148e456d184b3896f0fe99983672c95b9e
Nikhil Dhiman <nikhildhiman101@gmail.com>
2025-06-27 17:18:37 +0530

    Client: Implement Outlet Style

--- client/components/layout/Layout.jsx
+++ client/components/layout/Layout.jsx
@@ -1,12 +1,26 @@
 import { Outlet } from "react-router";
+import KeyRingIcon from "../../public/keyring.svg?react";
 
 export const Layout = () => {
 	return (
 		// add header here
-		<main className="bg-gray-800 text-slate-200">
-			<h1>Header</h1>
-			<Outlet />
-			<h1>Footer</h1>
+		<main className="flex flex-none flex-col dark:bg-gray-800 dark:text-light-grey min-w-screen min-h-screen">
+			{/* Header */}
+			<div className="flex flex-none items-center justify-center gap-x-1 text-light-grey p-2">
+				<KeyRingIcon className="w-9 h-9" />
+				<span className="font-thin text-2xl">
+					<span className="font-bold text-2xl">key</span>Ring
+				</span>
+			</div>
+			<div className="grow">
+				<Outlet />
+			</div>
+			{/* Footer */}
+			<div className="flex flex-none justify-center items-center p-2">
+				<h1>
+					Made By <span className="text-blue-300">Nikhil Dhiman</span>
+				</h1>
+			</div>
 		</main>
 		// add footer here
 	);
